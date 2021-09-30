7872c9c4187dbc1bff43949d03f976393d2512d5
ProkopaloA <83669029+ProkopaloA@users.noreply.github.com>
2021-09-30 11:51:32 +0200

    Update task2.js

--- JavaScriptLesson1/public/task2.js
+++ JavaScriptLesson1/public/task2.js
@@ -7,15 +7,15 @@ let password;
 login = "Admin";
 password = "TheMaster";
 
-if (login == "Admin") {
-	if (password == "TheMaster") {
+if (login === "Admin") {
+	if (password === "TheMaster") {
 		console.log("Welcome");
-	} else if (password == "Other") {
+	} else if (password === "Other") {
 		console.log("Wrong password");
 	} else {
 		console.log("Canceled");
 	}
-} else if (login == "Other") {
+} else if (login === "Other") {
 	console.log("I don't know you");
 } else {
 	console.log("Canceled");
